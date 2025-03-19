f6b400f953842d81dd25543396fdc2290bf1a2e9
Redcansigma <jakecabiac@gmail.com>
2025-03-19 16:40:24 +0800

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -124,10 +124,10 @@ var apps = [
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Poki_logo.svg/220px-Poki_logo.svg.png"
     },
     {
-      'id': 'instagram',
-      'title': 'Instagram',
-      'url': 'https://www.instagram.com',
-      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/130px-Instagram_logo_2022.svg.png"
+      'id': 'totallyscience',
+      'title': 'Totally Science',
+      'url': 'https://totallyscience.co',
+      "image": "https://roslyngamehub.vercel.app/assets/images/logo.png"
     },
     {
       'id': 'eaglecraft',
