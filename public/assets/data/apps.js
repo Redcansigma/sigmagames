39d8fab4f410400f2bbcec14074e77184f0f4188
Redcansigma <jakecabiac@gmail.com>
2025-03-19 16:11:52 +0800

    Update apps.js
    Added instagram

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -123,6 +123,12 @@ var apps = [
       'url': 'https://poki.com',
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Poki_logo.svg/220px-Poki_logo.svg.png"
     },
+    {
+      'id': 'instagram',
+      'title': 'Instagram',
+      'url': 'https://www.instagram.com',
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/130px-Instagram_logo_2022.svg.png"
+    },
   ]
   
   var customapps = JSON.parse(localStorage.getItem("customapps"))
